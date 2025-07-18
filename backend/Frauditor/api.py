4b5d95cb55cd9645ad2fd05e3bfdfca68a29cb85
tjtoo04 <ttjiat@gmail.com>
2025-07-18 12:13:51 +0800

    frauditor basic stuff done

--- backend/Frauditor/api.py
+++ backend/Frauditor/api.py
@@ -1,8 +1,10 @@
 from user.api import router as user_router
 from scraper.api import router as scraper_router
+from review.api import router as review_router
 from ninja import NinjaAPI
 
 api = NinjaAPI()
 
 api.add_router("users", user_router)
 api.add_router("scraper", scraper_router)
+api.add_router("reviews", review_router)
